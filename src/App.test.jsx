f6cf7a049925846f6412d8d189f10369d7a02b31
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-05-04 21:21:52 -0700

    clean up

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
-import CharacterList from './components/CharacterList';
 
 describe('App', () => {
   it('displays a list of character details', async () => {
